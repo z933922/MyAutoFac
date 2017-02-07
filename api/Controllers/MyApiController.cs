64b67092c8a536239344870f53696c955434dc78
z933922 <xiangyueqixi@foxmail.com>
2017-02-07 14:17:38 +0800

    api 路由

--- api/Controllers/MyApiController.cs
+++ api/Controllers/MyApiController.cs
@@ -8,11 +8,12 @@ using System.Web.Http;
 namespace api.Controllers
 {
 
-    //[RoutePrefix("api/MyApi")]
+    [RoutePrefix("api")]   //     [RouteArea("Admin")]这个的目的是 这个路由的前缀是 api/myapi
+
     public class MyApiController : ApiController
     {
 
-       [HttpGet]
+        [HttpGet]
         public string Index()
         {
             return "12321";
@@ -27,11 +28,24 @@ namespace api.Controllers
 
 
 
-        [HttpGet,Route("aa/{dd?}")]
+        [HttpGet, Route("aa/{dd?}")]
         public string GetValue()
         {
             return "this is new string";
         }
 
+        [HttpGet, Route("GG/{dd=xxoo}/{ee=eeeeee}")]
+        public string GetDefault(string dd, string ee)
+        {
+            return "dd:" + dd + " ee:" + ee;
+        }
+
+        [HttpGet]
+        [Route("{num:int:min(10)}")]
+        
+        public string yueshu(int num=23)
+        {
+            return num.ToString();
+        }
     }
 }
